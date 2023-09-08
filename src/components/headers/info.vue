465b98059cb59ce6b1c8b1d6de7e9ba4475cdf70
paulashjuanauo <139542978+paulashjuanauo@users.noreply.github.com>
2023-09-08 16:11:49 +0800

    Add files via upload

---
+++ src/components/headers/info.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { computed } from "vue"
+
+const props = defineProps({
+  devices: {
+    type: Array,
+    required: true,
+    default: [],
+  },
+})
+
+const deviceCount = computed(() => props.devices.length)
+const noDevice = computed(() => deviceCount.value === 0)
+</script>
+
+<template>
+  <h3 v-if="noDevice">目前無任何設備</h3>
+  <h3 v-else>目前共計 {{ deviceCount }} 項設備</h3>
+</template>
---
+++ src/components/todo/list.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+  device: {
+    type: Object,
+    required: true,
+  },
+})
+
+const emits = defineEmits(["remove-auo-item"])
+
+const remove = () => {
+  emits("remove-auo-item", props.device.id)
+}
+</script>
+
+<template>
+  <li
+    class="px-10 py-12 bg-white">
+    {{ device.title }}
+    <button class="px-2 py-1 text-white bg-black" @click="remove">X</button>
+  </li>
+</template>
